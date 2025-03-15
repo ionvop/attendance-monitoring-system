e9fe795845219694e24215d1a9f5c698937fd594
ionvop <ionvop@gmail.com>
2025-03-15 13:29:07 +0800

    Update common.php

--- common.php
+++ common.php
@@ -52,3 +52,20 @@ function Alert($message, $redirect = "") {
 
     exit();
 }
+
+function Icon($icon) {
+    switch ($icon) {
+        case "delete":
+            return <<<HTML
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/></svg>
+            HTML;
+        case "file_save":
+            return <<<HTML
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M560 0v-80h320V0H560Zm160-120L560-280l56-56 64 64v-167h80v167l64-64 56 56-160 160Zm-480-40q-33 0-56.5-23.5T160-240v-560q0-33 23.5-56.5T240-880h280l240 240v121H480v359H240Zm240-440h200L480-800v200Z"/></svg>
+            HTML;
+        case "remove":
+            return <<<HTML
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M200-440v-80h560v80H200Z"/></svg>
+            HTML;
+    }
+}
